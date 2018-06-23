5ce5acc6705259072ea808847ecba0b32133e983
Veronika Hanzlikova <hanzlikova@shoptet.cz>
2018-06-23 13:31:32 +0200

    blog.shoptet.cz wp template

--- home-page.php
+++ home-page.php
@@ -1,11 +1,14 @@
 <?php /* Template Name: Blog Home Page template */ ?>
 <?php get_header(); ?>
-
+<section class="section section-hidden">
+    <div class="section-inner container">
+        <?php the_title( '<h1>', '</h1>' ); ?>
+    </div>
+</section>
 <section class="section section-primary">
     <div class="section-inner container">
         <?php
-
-        query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged'));
+        query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('page'));     
 
         if ( have_posts() ) :
             while ( have_posts() ) : the_post();
