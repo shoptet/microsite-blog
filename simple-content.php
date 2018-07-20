dce8b0ee052c70ebde6236aaa1e0880a877185be
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2018-07-20 14:07:50 +0200

    Cleanup after shoptet-wp-theme refactoring

--- simple-content.php
+++
@@ -1,5 +0,0 @@
-<?php /* Template Name: Simple Content template */ ?>
-
-<h2><?php echo apply_filters( 'get_the_title', $page->post_title ); ?></h2>
-
-<?php echo apply_filters( 'the_content', $page->post_content ); ?>
