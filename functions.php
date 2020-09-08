37b340750d6a7ecc871fd15142d5a8eb9a8c71ef
Václav Vracovský <vracovsky@shoptet.cz>
2020-09-08 09:07:07 +0200

    SK language for blog

--- functions.php
+++ functions.php
@@ -4,6 +4,9 @@ function resolveLanguage()
     if (strpos($_SERVER["SERVER_NAME"], 'blog.shoptet.hu') !== FALSE) {
         return 'hu_HU';
     }
+    if (strpos($_SERVER["SERVER_NAME"], 'blog.shoptet.sk') !== FALSE) {
+        return 'sk_SK';
+    }
     return 'cs_CZ';
 };
 add_filter('locale', 'resolveLanguage');
